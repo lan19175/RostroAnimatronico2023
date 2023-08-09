ce12bf664d8d141529e53c8a02059f62cbbe96f4
Jorge Lanza <lan19175@uvg.edu.gt>
2023-08-09 13:29:25 -0600

    Diseño 1 sliders. Func initial values

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from kivy.app import App
-# from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from kivy.core.window import Window
 from pygrabber.dshow_graph import FilterGraph
+
 Window.maximize()
+
 Builder.load_file('templates/manager_window.kv')
 Builder.load_file('templates/mainWindow/main_window.kv')
 Builder.load_file('templates/dataSettingWindow/data_setting_window.kv')
@@ -38,9 +39,21 @@ class MainWindow(Screen):
 
 
 class MotorWindow(Screen):
+    def on_pre_enter(self):
+        self.initial_values()
+
+    def initial_values(self):
+        for i in range(11):
+            id_slider = "slider_servo" + str(i)
+            id_text = "grados_servo" + str(i)
+            id_angle = "servo_movible" + str(i)
+            value = int(self.ids[str(id_slider)].value)
+            self.ids[str(id_text)].text = f'{int(value)}°'
+            self.ids[str(id_angle)].servo_angle = int(value)
+
     def servo_screen(self, row, col):
-        pos_extra_x = col*.192
-        pos_extra_y = row*.26
+        pos_extra_x = col * .192
+        pos_extra_y = row * .26
         window_size = Window.size
         x_pos = (pos_extra_x + .295) * window_size[0]
         y_pos = (.71 - pos_extra_y) * window_size[1]
@@ -52,6 +65,12 @@ class MotorWindow(Screen):
         id_angle = "servo_movible" + str(no_servo)
         self.ids[str(id_text)].text = f'{int(value)}°'
         self.ids[str(id_angle)].servo_angle = int(value)
+
+    def initial_value(self, no_servo):
+        value = [180, 50, 30, 90,
+                 55, 95, 150, 10,
+                 20, 160, 120, 45]
+        return int(value[no_servo])
 
 
 class MotorDataSettingWindow(Screen):
