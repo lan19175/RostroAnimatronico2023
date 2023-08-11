449f57d4b983958e3699f868012657d44a47df62
Jorge Lanza <lan19175@uvg.edu.gt>
2023-08-11 13:58:37 -0600

    Reorganizacion img old/new, Settings como Popup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.popup import Popup
 from kivy.lang import Builder
 from kivy.core.window import Window
 from pygrabber.dshow_graph import FilterGraph
@@ -10,14 +11,14 @@ Builder.load_file('templates/manager_window.kv')
 Builder.load_file('templates/mainWindow/main_window.kv')
 Builder.load_file('templates/dataSettingWindow/data_setting_window.kv')
 Builder.load_file('templates/motorWindow/motor_window.kv')
-Builder.load_file('templates/settingsWindow/settings_window.kv')
+Builder.load_file('templates/settingsWindow/settings_popup.kv')
 
 
 class WindowManager(ScreenManager):
     pass
 
 
-class SettingWindow(Screen):
+class SettingWindow(Popup):
     def spinner_clicked(self, value):
         pass
 
@@ -43,7 +44,7 @@ class MotorWindow(Screen):
         self.initial_values()
 
     def initial_values(self):
-        for i in range(12):
+        for i in range(19):
             id_slider = "slider_servo" + str(i)
             id_text = "grados_servo" + str(i)
             id_angle = "servo_movible" + str(i)
@@ -61,11 +62,13 @@ class MotorWindow(Screen):
         return pos
 
     def servos_initial_value(self, no_servo):
-        value = [180, 50, 30, 90,
-                 55, 95, 150, 10,
-                 20, 160, 120, 45,
-                 70, 25, 37, 123,
-                 4, 175, 21]
+        value = [180, 50, 30,
+                 90, 55, 95,
+                 150, 10, 20,
+                 160, 120, 45,
+                 70, 300, 180,
+                 300, 4, 225,
+                 45]
         return int(value[no_servo])
 
     def slider_func(self, no_servo, value):
